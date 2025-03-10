a06ebd713cf2a81eb2156e52e5b2b25325d17ad1
Jebbe9 <jebbede@protonmail.com>
2025-03-10 23:03:11 +0200

    Font & pywal

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Terminus:pixelsize=12:antialias=false:autohint=false";
 static int borderpx = 2;
 
 /*
@@ -94,11 +94,11 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.7;
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
+/*static const char *colorname[] = {
+	   8 normal colors 
 	"black",
 	"red3",
 	"green3",
@@ -108,7 +108,7 @@ static const char *colorname[] = {
 	"cyan3",
 	"gray90",
 
-	/* 8 bright colors */
+	   8 bright colors 
 	"gray50",
 	"red",
 	"green",
@@ -120,22 +120,25 @@ static const char *colorname[] = {
 
 	[255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
+	   more colors can be added after 255 to use with DefaultXX 
 	"#cccccc",
 	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
-};
+	"gray90",    default foreground colour 
+	"black",    default background colour 
+};*/
 
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
+/*unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
 unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultrcs = 257;*/
+
+#include "/home/jere/.cache/wal/colors-wal-st.h"
+
 
 /*
  * Default shape of cursor
